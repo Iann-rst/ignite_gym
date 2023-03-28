f5a012c599f2bb385809a4d446818c4f385711a0
Iann Rodrigues <iannrst11@gmail.com>
2023-03-28 11:06:50 -0300

    feat: Definindo a imagem padrão

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -22,6 +22,7 @@ export function SignIn() {
       <VStack flex={1} px={10} pb={16}>
         <Image
           source={BackGroundImg}
+          defaultSource={BackGroundImg}
           alt="Pessoas treinando"
           resizeMode="contain"
           position="absolute"
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -21,6 +21,7 @@ export function SignUp() {
       <VStack flex={1} px={10} pb={16}>
         <Image
           source={BackGroundImg}
+          defaultSource={BackGroundImg}
           alt="Pessoas treinando"
           resizeMode="contain"
           position="absolute"
