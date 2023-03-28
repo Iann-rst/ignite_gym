c529e66b883cb16eedd6e5a8add9c4499cc1bde0
Iann Rodrigues <iannrst11@gmail.com>
2023-03-28 13:04:12 -0300

    feat: Componente Header da interface Home

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,30 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import { Heading, HStack, Icon, Text, VStack } from 'native-base';
+import { TouchableOpacity } from 'react-native';
+import { UserAvatar } from './UserAvatar';
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserAvatar
+        source={{ uri: 'https://github.com/Iann-rst.png' }}
+        alt="Imagem do usuário"
+        size={16}
+        mr={4}
+      />
+
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
+        <Heading color="gray.100" fontSize="md">
+          Iann
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  )
+}
