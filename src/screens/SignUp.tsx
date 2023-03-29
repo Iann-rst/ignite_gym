81a5b8dc4d5d2f659e5222fd5b20960a39a13d24
Iann Rodrigues <iannrst11@gmail.com>
2023-03-29 18:55:26 -0300

    feat: Validação com React Hook Form e Zod

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,7 +1,8 @@
-import { Center, Heading, Image, ScrollView, Text, VStack } from "native-base";
-
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useNavigation } from "@react-navigation/native";
-
+import { Center, Heading, Image, ScrollView, Text, VStack } from "native-base";
+import { Controller, useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 import BackGroundImg from '@assets/background.png';
 import LogoSvg from "@assets/logo.svg";
@@ -9,11 +10,51 @@ import { Button } from "@components/Button";
 import { Input } from "@components/Input";
 
 
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+}
+
+// schema de validação do formulário com o Zod
+const schemaValidation = z.object({
+  name: z.string({
+    required_error: 'Informe o nome.'
+  }).trim().nonempty({
+    message: 'Informe o nome.'
+  }),
+
+  email: z.string({
+    required_error: 'Informe o email.'
+  }).email('E-mail inválido.'),
+
+  password: z.string({
+    required_error: 'Informe a senha.'
+  }).min(6, 'A senha deve ter pelo menos 6 dígitos.'),
+
+  password_confirm: z.string({
+    required_error: 'Confirme a senha.'
+  }).min(6, 'A senha deve ter pelo menos 6 dígitos.')
+
+}).refine((data) => data.password === data.password_confirm, {
+  path: ["password_confirm"],
+  message: "Senhas diferentes"
+})
+
 export function SignUp() {
   const { goBack } = useNavigation();
 
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
+    resolver: zodResolver(schemaValidation)
+  });
+
   function handleGoBack() {
     goBack()
+  }
+
+  function handleSignUp(data: FormDataProps) {
+    console.log("Formulário => ", data);
   }
 
   return (
@@ -40,28 +81,77 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input
-            placeholder="Nome"
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Nome"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.name?.message}
+              />
+            )}
           />
 
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
+
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.email?.message}
+              />
+            )}
           />
 
-          <Input
-            placeholder="Senha"
-            secureTextEntry
+
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.password?.message}
+              />
+            )} />
+
+
+          <Controller
+            control={control}
+            name="password_confirm"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Confirme a senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType="send"
+                errorMessage={errors.password_confirm?.message}
+              />
+            )} />
+
+
+          <Button
+            title="Criar e acessar"
+            onPress={handleSubmit(handleSignUp)}
           />
 
-          <Button title="Criar e acessar" />
         </Center>
 
         <Button
           title="Voltar para o login"
           variant="outline"
-          mt={24}
+          mt={12}
           onPress={handleGoBack}
         />
       </VStack>
