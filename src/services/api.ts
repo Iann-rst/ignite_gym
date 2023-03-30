f79a62924a486fca014ea2d28558467eea1c1b6c
Iann Rodrigues <iannrst11@gmail.com>
2023-03-30 15:59:27 -0300

    feat: Criando Interceptores

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,28 @@
 import axios from "axios";
+import { AppError } from './../utils/AppError';
 
 
-export const api = axios.create({
+const api = axios.create({
   baseURL: 'http://10.0.0.143:3333'
 })
+
+/*
+
+api.interceptors.request.use((config) => {
+  console.log("DADOS ENVIADOS", config.data)
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+})
+
+*/
+
+api.interceptors.response.use(response => response, error => {
+  if (error.response && error.response.data) {
+    return Promise.reject(new AppError(error.response.data.message));
+  } else {
+    return Promise.reject(error)
+  }
+});
+
+export { api };
