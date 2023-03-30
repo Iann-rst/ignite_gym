23b4f8375557f8ec3b200f5b5d17b31dcae258b4
Iann Rodrigues <iannrst11@gmail.com>
2023-03-30 19:33:37 -0300

    feat: Rotas de acordo se há ou não usuário

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
+import { useAuth } from "@hooks/useAuth";
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 
 import { Box, useTheme } from 'native-base';
 import { AppRoutes } from "./app.routes";
+import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
   const { colors } = useTheme();
+
+  const { user } = useAuth();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700]
@@ -12,7 +16,7 @@ export function Routes() {
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        <AppRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   )
