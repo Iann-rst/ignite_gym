7ed27f7b2994e69624bed44286c8114b67b50188
Iann Rodrigues <iannrst11@gmail.com>
2023-03-30 19:48:27 -0300

    feat: Persistindo usuário no async storage

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,8 @@
 import { UserDTO } from "@dtos/UserDTO";
 import { api } from "@services/api";
+import { storageUserSave } from "@storage/storageUser";
 import { createContext, ReactNode, useState } from "react";
+
 
 export type AuthContextDataProps = {
   user: UserDTO;
@@ -24,6 +26,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
       if (data.user) {
         setUser(data.user)
+        storageUserSave(data.user);
       }
     } catch (error) {
       throw error;
