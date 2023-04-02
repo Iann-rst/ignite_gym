908ab725da2668ac7a47af29f9b36701b62d919e
Iann Rodrigues <iannrst11@gmail.com>
2023-04-02 18:32:37 -0300

    feat: Input de email para usuário não digitar

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -197,6 +197,7 @@ export function Profile() {
                 isDisabled
                 onChangeText={onChange}
                 value={value}
+                isReadOnly
               />
             )}
           />
